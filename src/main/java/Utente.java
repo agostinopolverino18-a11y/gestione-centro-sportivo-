811b15e5f1c952ad9aef5b0785b8a1035149baa2
agostinopolverino18-a11y <agostinopolverino18@gmail.com>
2026-05-01 11:24:01 +0200

    Add files via upload

---
+++ src/main/java/Utente.java
@@ -0,0 +1,8 @@
+public abstract class Utente {
+    protected int id;
+    protected String nome;
+    protected String email;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}

f71eb7199f842fa51e193eba3f55dcc2ba86a19a
agostinopolverino18-a11y <agostinopolverino18@gmail.com>
2026-05-03 18:41:48 +0200

    Delete src/main/java directory

--- src/main/java/Utente.java
+++
@@ -1,8 +0,0 @@
-public abstract class Utente {
-    protected int id;
-    protected String nome;
-    protected String email;
-
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-}
